09fa39519ee874a33827933c0c98ec9cd08f2463
Sakar Vaidya <sakarvaidya72527@gmail.com>
2025-08-12 13:54:14 +0530

    location icon

--- resources/views/locations/plant-tree.blade.php
+++ resources/views/locations/plant-tree.blade.php
@@ -200,10 +200,11 @@ window.initMap = function initMap() {
         position: currentCoords,
         map: map,
         draggable: true,
-        title: 'Tree Planting Spot',
+        title: 'Tree Planting Location',
         icon: {
-            url: 'data:image/svg+xml;charset=UTF-8,<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="%2300A000"><path d="M12 2L13.09 8.26L20 9L13.09 9.74L12 16L10.91 9.74L4 9L10.91 8.26L12 2Z"/></svg>',
-            scaledSize: new google.maps.Size(32, 32)
+            url: 'data:image/svg+xml;charset=UTF-8,<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="%23DC3545"><path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z"/></svg>',
+            scaledSize: new google.maps.Size(32, 32),
+            anchor: new google.maps.Point(16, 32)
         }
     });
 
